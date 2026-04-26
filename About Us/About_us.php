de32ff3be14e7f70379f7042290e565bf68f2b18
priyarishu082-boop <priyarishu082@gmail.com>
2026-04-26 14:42:09 +0530

    Add files via upload

---
+++ About Us/About_us.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="About.css">
+</head>
+
+<body>
+    <header class="header">
+        <div class="logo"><img src="../assets/Logo.png" alt="HMS" width="80px"></div>
+
+        <h1>HOSPITAL MANAGEMENT SYSTEM</h1></br>
+        <!-- <p>“Complete care in just one click”</p> -->
+    </header>
+
+    <nav class="top-nav">
+        <ul>
+            <li><a href="../Homepage.php">Home</a></li>
+            <li><a href="../About Us/About_us.php">About us</a></li>
+            <li><a href="../Doctor/Doctor.php">Doctor</a></li>
+            <li><a href="../Patient/Patient.php">Patient</a></li>
+            <li><a href="../Lab/Lab.php">Lab</a></li>
+            <li><a href="../Services/Services.php">Services</a></li>
+            <!-- <li><a href="#"><i class="fa-solid fa-right-from-bracket"></i>Logout</a></li> -->
+        </ul>
+    </nav>
+
+    <div class="main">
+
+        <h2 class="main-title">WELCOME! TO HOSPITAL MANAGEMENT SYSTEM</h2>
+        <!-- <h3>OUR MOTTO OUR VISION</h3> -->
+
+        <div class="img">
+         <img src="../assets/mission.png" alt="mission">
+        </div>
+
+        <p class="para">Our mission our vision &starf; <u>“Complete care in just one click”</u></p>
+        <p class="para">The “HOSPITAL MANAGEMENT SYSTEM” has been developed to reduces paperwork, minimizes error and
+            improves
+            the efficiency of hospital system.
+            It enables faster retrieval of patient records , better coordination between doctors and patients and
+            smooth handling of daily hospital activities. The proposed system provides an organized, reliable and
+            efficient way to manage hospital data , ensuring data consistency. </p>
+        <p class="para">
+            As we know, hospital helps us by offering the greatest medical care to those with a variety of
+            illnesses, whether brought on by stress, emotional trauma, a change in the weather or the other causes.
+            Sole purpose of this project is to avoid local people or citizen from daily rush and to introduce a
+            system so that they can easily use it and benefits it. This project aims to develop an efficient and
+            user-friendly system.
+            Designed with the vision of motto complete care in just one click , provides complete care through
+            secure , scalable and flexibility.
+        </p>
+
+
+        <h2 class="main-title">OUR OBJECTIVES</h2>
+        <p class="para">The name of my project is “HOSPITAL MANAGEMENT SYSTEM”. My aim behind creating this project is
+            to
+            integrate with the real world problem.
+            The main objective of this project is to manage the details of hospitals, patients, doctors, lab
+            ,reports and their appointment.
+            The sole purpose of this project is to reduce the manual work for managing hospitals and to design and
+            implement an efficient and user friendly system , so that local people or citizen can easily find the
+            information about the hospital and use it.
+        </p>
+
+
+        <h2 class="main-title">PROJECT CATEGORY</h2>
+        <p class="para">This project is categorized as a web-based application and falls under category of a Database
+            Management System (DBMS), as it involves designing and maintaining structured database to store, retrieve
+            and manage large number of data efficiently.
+
+            The system ensures data consistency, data integrity and secure access through defined database constraints
+            and access control mechanisms</p>
+    </div>
+
+    <!-- EVENTS SECTIONS -->
+
+    <h2 class="main-title">EVENTS AND ACHIEVEMENTS</h2>
+    <div class="img">
+    
+        <img src="../assets/achievement1.webp" width="30%">
+        <img src="../assets/image.png" width="30%">
+        <img src="../assets/hospitalimage.avif" width="30%">
+        
+    </div>
+
+    <!-- FOOTER -->
+    <footer>
+        <div class="footer">
+            <div class="Address">
+                <h4><i class="fa-solid fa-location-dot"></i> Address</h4>
+                <p>XYZ Deoghar, jharkhand</p>
+                <p>India, PIN-814152</p>
+            </div>
+
+            <div class="Help">
+                <h4><i class="fa-brands fa-hire-a-helper"></i> Help Desk</h4>
+                <p>06778987373</p>
+                <p>saturday- 8:30AM to 4:30 PM</p>
+                <p>Sunday/Holiday - Closed</p>
+            </div>
+
+            <div class="Email">
+                <h4><i class="fa-solid fa-envelope"></i> Email Address</h4>
+                <p>admin@hospital.edu.in</p>
+                <p>www.hospitalmanage.edu.in</p>
+            </div>
+
+        </div>
+
+        <div class="hms">
+            <p>&copy; 2026 | HOSPITAL MANAGEMENT SYSTEM | Web-development made with &hearts; by Rishu Priya</p>
+        </div>
+
+
+    </footer>
+</body>
+
+</html>
